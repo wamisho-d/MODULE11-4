cedede10c4028f808fc0ce88b4f35307f020b55d
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2025-02-07 19:57:45 -0500

    Update # 2 App.jsx

--- comic-book-library/src/# 2 App.jsx
+++ comic-book-library/src/# 2 App.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, NavLink } from 'react-router-dom';
 import Home from './components/Home';
 import BrowseCharacters from './components/BrowseCharacters';
 import CharacterDetails from './components/CharacterDetails';
 import Comics from './components/Comics';
+import NotFound from './components/NotFound'; // Import the NotFound component
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/browse-characters" element={<BrowseCharacters />} />
-            <Route path="/character-details" element={<CharacterDetails />} />
-            <Route path="/comics" element={<Comics />} />
-        </Routes>
+        <div>
+            <nav>
+                <NavLink to="/" end activeClassName="active">Home</NavLink>
+                <NavLink to="/browse-characters" activeClassName="active">Browse Characters</NavLink>
+                <NavLink to="/comics" activeClassName="active">Comics</NavLink>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/browse-characters" element={<BrowseCharacters />} />
+                <Route path="/character-details/:id" element={<CharacterDetails />} />
+                <Route path="/comics" element={<Comics />} />
+                <Route path="*" element={<NotFound />} /> {/* Handle undefined routes */}
+            </Routes>
+        </div>
     );
 };
 
