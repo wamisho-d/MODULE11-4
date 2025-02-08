3c5e6b3205fb48f73a076f739f1eb2f32da11a87
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2025-02-07 19:53:22 -0500

    Update # 1 CharacterDetails.jsx

---
+++ comic-book-library/src/component/# 1 CharacterDetails.jsx
@@ -0,0 +1,10 @@
+const CharacterDetails = () => {
+    return (
+        <div>
+            <h2>Character Details</h2>
+            <p>Details about a specific character will be displayed here.</p>
+        </div>
+    );
+};
+
+export default CharacterDetails;
