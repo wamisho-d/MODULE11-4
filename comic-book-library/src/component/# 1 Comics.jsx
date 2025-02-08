7f6ea49cd6b2128d31f58c12e4d9c1c746e405ee
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2025-02-07 19:53:51 -0500

    Update # 1 Comics.jsx

---
+++ comic-book-library/src/component/# 1 Comics.jsx
@@ -0,0 +1,10 @@
+const Comics = () => {
+    return (
+        <div>
+            <h2>Comics</h2>
+            <p>This component is under construction. Check back later!</p>
+        </div>
+    );
+};
+
+export default Comics;
